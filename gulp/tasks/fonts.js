ffaf262dd02d9682fba7838d93130c073237a528
TheMarchell0 <TheMarchell0@yandex.ru>
2024-12-31 21:01:38 +0600

    change style file structure

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -1,11 +1,9 @@
 import gulp from 'gulp';
-import { app } from '../gulpfile.js'; // Импортируем app из gulpfile.js
+import { app } from '../gulpfile.js';
 
-// Задача для копирования шрифтов
 export const fonts = () => {
-    return gulp.src(app.path.src.fonts) // Путь к папке с шрифтами в src
-        .pipe(gulp.dest(app.path.build.fonts)); // Путь, куда будут скопированы шрифты в dist
+    return gulp.src(app.path.src.fonts)
+        .pipe(gulp.dest(app.path.build.fonts));
 };
 
-// Экспортируйте задачу, чтобы ее можно было использовать в других задачах
 export default fonts;
