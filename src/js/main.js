6097f351f14d950530cd24e7dacb8673f516bfec
TheMarchell0 <TheMarchell0@yandex.ru>
2025-01-07 13:16:19 +0600

    try to fix empty bg in production

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,9 @@ import {componentsFunctions} from "./helpers/_components.js";
 import {createSelect} from "./functions/createSelect.js";
 import {modalFunctional} from "./functions/createModalFunctional.js";
 import {fixedHeaderFunctional} from "./functions/createFixedHeaderFunctional.js";
-import {GSAPAnimations} from "./functions/createGSAPAnimations.js";
 import {createInputPlaceholderActivity} from "./functions/createInputPlaceholderActivity.js";
 
 document.addEventListener("DOMContentLoaded", function () {
-    GSAPAnimations();
     fixedHeaderFunctional();
     createSelect();
     componentsFunctions();
